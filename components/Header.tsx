65a1e5ed3b50201f8d19b4dc26eb67e12b84f710
ismailtarihc <96085756+BeneiElohim@users.noreply.github.com>
2023-10-27 12:01:02 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
+const Modal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+        <p className="mb-4 text-center">
+          Çalışma altında :) Lütfen tescil tarama kısmından devam edin.
+        </p>
+        <button
+          onClick={onClose}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mx-auto block">
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
 const Header = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
     <header className="flex justify-between items-center py-4 px-6 bg-white border-b-4 border-r-1 border-l-1 border-quinary">
       <div className="flex items-center">
@@ -19,7 +39,9 @@ const Header = () => {
             Tescil Taraması
           </button>
         </Link>
-        <button className="bg-secondary hover:bg-tertiary text-gray-800 hover:text-quaternary font-semibold py-2 px-4 border border-gray-400 rounded shadow ml-2">
+        <button
+          onClick={() => setModalOpen(true)}
+          className="bg-secondary hover:bg-tertiary text-gray-800 hover:text-quaternary font-semibold py-2 px-4 border border-gray-400 rounded shadow ml-2">
           Tescilini Yükle
         </button>
       </div>
@@ -29,6 +51,7 @@ const Header = () => {
         </button>
         <div className="h-8 w-8 bg-gray-400 rounded-full ml-4"></div>
       </div>
+      {isModalOpen && <Modal onClose={() => setModalOpen(false)} />}
     </header>
   );
 };
