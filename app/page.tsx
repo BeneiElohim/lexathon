25b32a8804481f7c0dba110e52e6165eb1dc5502
ismailtarihc <96085756+BeneiElohim@users.noreply.github.com>
2023-10-28 10:58:03 +0300

    pictures

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,36 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Lexathon Prototype</h1>
+    <main className="flex flex-col items-center">
+      {/* Image 1 */}
+      <div className="my-8">
+        <Image
+          src="/1.png"
+          alt="Description of Image 1"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      {/* Image 2 */}
+      <div className="my-8">
+        <Image
+          src="/2.png"
+          alt="Description of Image 2"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      {/* Image 3 */}
+      <div className="my-8">
+        <Image
+          src="/3.png"
+          alt="Description of Image 3"
+          width={500}
+          height={500}
+        />
+      </div>
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -28,26 +28,23 @@ const Header = () => {
         <Link href="/">
           <img className="w-32 h-auto mr-2" src="/logo.png" alt="Logo" />
         </Link>
-        <div>
-          <p className="text-lg font-semibold text-gray-900">StyLegally</p>
-          <p className="text-sm font-medium text-gray-600">Özgün ve Yasal</p>
-        </div>
+        <div></div>
       </div>
       <div className="flex">
         <Link href="/LacernoArama">
           <button className="bg-secondary hover:bg-tertiary text-gray-800 hover:text-quaternary font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-            Tescil Taraması
+            Tasarım Taraması
           </button>
         </Link>
         <button
           onClick={() => setModalOpen(true)}
           className="bg-secondary hover:bg-tertiary text-gray-800 hover:text-quaternary font-semibold py-2 px-4 border border-gray-400 rounded shadow ml-2">
-          Tescilini Yükle
+          Tasarımını Yükle
         </button>
       </div>
       <div className="flex items-center">
         <button className="bg-quinary hover:bg-white text-white hover:text-black font-semibold py-2 px-4 border  rounded shadow-md">
-          Çıkış Yap
+          Bülten Alarmı
         </button>
         <div className="h-8 w-8 bg-gray-400 rounded-full ml-4"></div>
       </div>
